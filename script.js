5fc97544b6381f6aa90f93b4dce21044c2a5133b
SaraZaskorska <89976400+SaraZaskorska@users.noreply.github.com>
2023-11-29 11:34:02 +0100

    zmiana funkcji na strzałkowe

--- script.js
+++ script.js
@@ -1,83 +1,95 @@
 const apiUrl = 'https://hacker-news.firebaseio.com/v0/';// Adres API Hacker News
 const maxNewsPerPage = 30;// Maksymalna liczba newsów na stronie
 let currentPage = 1;// Numer aktualnej strony
-let isVoteButtonGreen = false;// Dodaj zmienną do śledzenia stanu przycisku
+let isVoteButtonGreen = false;// Zmienna do śledzenia stanu przycisku
 
-async function fetchNews(page) {// Funkcja asynchroniczna do pobierania newsów z danej strony
-    const response = await fetch(`${apiUrl}newstories.json`);// Pobranie listy ID najnowszych newsów
-    const data = await response.json();    
-    const startIndex = (page - 1) * maxNewsPerPage;// Obliczenie zakresu indeksów dla danej strony
-    const endIndex = startIndex + maxNewsPerPage;   
-    const newsIds = data.slice(startIndex, endIndex);// Wybranie ID newsów dla danej strony    
-    const newsPromises = newsIds.map(newsId => fetch(`${apiUrl}item/${newsId}.json`).then(response => response.json()));// Pobranie szczegółowych danych dla każdego ID newsa    
-    const newsList = await Promise.all(newsPromises);// Oczekiwanie na zakończenie wszystkich żądań
+
+const fetchNews = async (page) => {// Funkcja asynchroniczna do pobierania newsów z danej strony
+ 
+    const response = await fetch(`${apiUrl}newstories.json`);   // Pobranie listy ID najnowszych newsów
+    const data = await response.json();
+    const startIndex = (page - 1) * maxNewsPerPage;   // Obliczenie zakresu indeksów dla danej strony
+    const endIndex = startIndex + maxNewsPerPage;
+    const newsIds = data.slice(startIndex, endIndex);// Wybranie ID newsów dla danej strony
+    const newsPromises = newsIds.map(newsId => fetch(`${apiUrl}item/${newsId}.json`).then(response => response.json()));// Pobranie szczegółowych danych dla każdego ID newsa
+    const newsList = await Promise.all(newsPromises);
     return newsList;
-}
-function displayNews(newsList) {// Funkcja do wyświetlania newsów na stronie
+};
+
+
+const displayNews = (newsList) => {// Funkcja do wyświetlania newsów na stronie
     const newsListContainer = document.getElementById('news-list');
     newsListContainer.innerHTML = '';// Wyczyszczenie aktualnej listy przed dodaniem nowych newsów
+    
     newsList.forEach((news, index) => {
         const newsItem = document.createElement('li');
         newsItem.classList.add('news-item');
-
-        const voteButton = document.createElement('span');// Przycisk do głosowania
+        const voteButton = document.createElement('span');
         voteButton.classList.add('vote-up');
-        voteButton.style.color = isVoteButtonGreen ? 'green' : 'black';
+        
+        voteButton.style.color = isVoteButtonGreen ? 'green' : 'black';// Ustawienie koloru trójkąta na zielony lub czarny w zależności od stanu
         voteButton.innerHTML = ` ▲`;
+
+        // Obsługa głosowania i zmiana koloru trójkąta po naciśnięciu
         voteButton.addEventListener('click', () => {
             voteUp(index + 1);
-            
-            // Zmiana stanu przycisku po naciśnięciu
-            isVoteButtonGreen = !isVoteButtonGreen;
-
-            // Aktualizacja koloru trójkąta
-            voteButton.style.color = isVoteButtonGreen ? 'green' : 'black';
+            isVoteButtonGreen = !isVoteButtonGreen;// Zmiana stanu przycisku po naciśnięciu
+            voteButton.style.color = isVoteButtonGreen ? 'green' : 'black';// Aktualizacja koloru trójkąta
         });
 
-        const hideButton = document.createElement('span');// Przycisk do ukrywania newsa
+        const hideButton = document.createElement('span');
         hideButton.classList.add('hide-button');
         hideButton.innerHTML = 'Hide';
-        hideButton.addEventListener('click', () => hideNews(newsItem));
+        hideButton.addEventListener('click', () => hideNews(newsItem));// Ukrywanie newsa po naciśnięciu przycisku "Hide"
 
-        const newsTitle = document.createElement('div');// Tytuł i link do newsa
+        const newsTitle = document.createElement('div');
         newsTitle.classList.add('news-title');
         const newsLink = document.createElement('a');
         newsLink.classList.add('news-link');
         newsLink.href = news.url;
         newsLink.target = '_blank';
-        newsLink.innerHTML =` <span>${index + 1} </span>`+ news.title;
+        newsLink.innerHTML = `<span>${index + 1} </span>` + news.title;
         newsTitle.appendChild(voteButton);
         newsTitle.appendChild(newsLink);
 
-        const newsMeta = document.createElement('div');// Meta informacje o newsie (punkty, autor, liczba komentarzy)
+        const newsMeta = document.createElement('div');
         newsMeta.classList.add('news-meta');
         const newsMetaSpan = document.createElement('span');
         newsMetaSpan.innerHTML = `Score: ${news.score} | Author: ${news.by} | Comments: ${news.descendants} | `;
         newsMeta.appendChild(newsMetaSpan);
         newsMeta.appendChild(hideButton);
 
-        newsItem.appendChild(newsTitle); // Dodanie elementów do struktury HTML
+        newsItem.appendChild(newsTitle);
         newsItem.appendChild(newsMeta);
         newsListContainer.appendChild(newsItem);
     });
-}
-async function loadMoreNews() {// Funkcja asynchroniczna do ładowania kolejnych newsów
+};
+
+// Funkcja asynchroniczna do ładowania kolejnych newsów
+const loadMoreNews = async () => {
     currentPage++;
     const moreNews = await fetchNews(currentPage);
     displayNews(moreNews);
-}
-function voteUp(index) {// Funkcja do obsługi głosowania
+};
+
+// Funkcja do obsługi głosowania
+const voteUp = (index) => {
     console.log(`Voted up for news ${index}`);
-}
-function hideNews(newsItem) {// Funkcja do ukrywania newsa (Komentarz wygenerowany przez chatGPT)
+};
+
+// Funkcja do ukrywania newsa
+const hideNews = (newsItem) => {
     newsItem.hidden = true;
-}
-async function init() {// Inicjalizacja strony
+};
+
+// Inicjalizacja strony
+const init = async () => {
     const initialNews = await fetchNews(currentPage);
     displayNews(initialNews);
 
     const loadMoreButton = document.getElementById('load-more');
     loadMoreButton.addEventListener('click', loadMoreNews);
-}
+};
 
-init();// Wywołanie inicjalizacji po załadowaniu strony
+// Wywołanie inicjalizacji po załadowaniu strony
+init();
