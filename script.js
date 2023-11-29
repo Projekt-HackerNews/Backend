81cad8448b6885e9e85fcf266cd672b707ac7255
SaraZaskorska <89976400+SaraZaskorska@users.noreply.github.com>
2023-11-29 11:21:41 +0100

    poprawiony przycisk trójkąta

--- script.js
+++ script.js
@@ -1,87 +1,83 @@
-const apiUrl = 'https://hacker-news.firebaseio.com/v0/';
-const maxNewsPerPage = 30;
-let currentPage = 1;
+const apiUrl = 'https://hacker-news.firebaseio.com/v0/';// Adres API Hacker News
+const maxNewsPerPage = 30;// Maksymalna liczba newsów na stronie
+let currentPage = 1;// Numer aktualnej strony
+let isVoteButtonGreen = false;// Dodaj zmienną do śledzenia stanu przycisku
 
-    const fetchNews = async (page) => {
-        const response = await fetch(`${apiUrl}newstories.json`);
-        const data = await response.json();
-        const startIndex = (page - 1) * maxNewsPerPage;
-        const endIndex = startIndex + maxNewsPerPage;
-        const newsIds = data.slice(startIndex, endIndex);
-        const newsPromises = newsIds.map(newsId =>
-            fetch(`${apiUrl}item/${newsId}.json`).then(response => response.json())
-        );
-        const newsList = await Promise.all(newsPromises);
-        return newsList;
-    };
-    const displayNews = (newsList) => {
-        const newsListContainer = document.getElementById('news-list');
-        newsListContainer.innerHTML = '';
+async function fetchNews(page) {// Funkcja asynchroniczna do pobierania newsów z danej strony
+    const response = await fetch(`${apiUrl}newstories.json`);// Pobranie listy ID najnowszych newsów
+    const data = await response.json();    
+    const startIndex = (page - 1) * maxNewsPerPage;// Obliczenie zakresu indeksów dla danej strony
+    const endIndex = startIndex + maxNewsPerPage;   
+    const newsIds = data.slice(startIndex, endIndex);// Wybranie ID newsów dla danej strony    
+    const newsPromises = newsIds.map(newsId => fetch(`${apiUrl}item/${newsId}.json`).then(response => response.json()));// Pobranie szczegółowych danych dla każdego ID newsa    
+    const newsList = await Promise.all(newsPromises);// Oczekiwanie na zakończenie wszystkich żądań
+    return newsList;
+}
+function displayNews(newsList) {// Funkcja do wyświetlania newsów na stronie
+    const newsListContainer = document.getElementById('news-list');
+    newsListContainer.innerHTML = '';// Wyczyszczenie aktualnej listy przed dodaniem nowych newsów
+    newsList.forEach((news, index) => {
+        const newsItem = document.createElement('li');
+        newsItem.classList.add('news-item');
 
-        newsList.forEach((news, index) => {
-            const newsItem = document.createElement('li');
-            newsItem.classList.add('news-item');
+        const voteButton = document.createElement('span');// Przycisk do głosowania
+        voteButton.classList.add('vote-up');
+        voteButton.style.color = isVoteButtonGreen ? 'green' : 'black';
+        voteButton.innerHTML = ` ▲`;
+        voteButton.addEventListener('click', () => {
+            voteUp(index + 1);
+            
+            // Zmiana stanu przycisku po naciśnięciu
+            isVoteButtonGreen = !isVoteButtonGreen;
 
-            const voteButton = document.createElement('span');
-            voteButton.classList.add('vote-up');
-            voteButton.innerHTML = `${index + 1} ▲`;
-            voteButton.addEventListener('click', () => {
-                voteUp(index + 1);
-                highlightVoteButton(voteButton);
-            });
-
-            const hideButton = document.createElement('span');
-            hideButton.classList.add('hide-button');
-            hideButton.innerHTML = 'Hide';
-            hideButton.addEventListener('click', () => hideNews(newsItem));
-
-            const newsTitle = document.createElement('div');
-            newsTitle.classList.add('news-title');
-
-            const newsLink = document.createElement('a');
-            newsLink.classList.add('news-link');
-            newsLink.href = news.url;
-            newsLink.target = '_blank';
-            newsLink.innerHTML = news.title;
-            newsTitle.appendChild(voteButton);
-            newsTitle.appendChild(newsLink);
-
-            const newsMeta = document.createElement('div');
-            newsMeta.classList.add('news-meta');
-
-            const newsMetaSpan = document.createElement('span');
-            newsMetaSpan.innerHTML = `Score: ${news.score} | Author: ${news.by} | Comments: ${news.descendants} | `;
-            newsMeta.appendChild(newsMetaSpan);
-            newsMeta.appendChild(hideButton);
-            newsItem.appendChild(newsTitle);
-            newsItem.appendChild(newsMeta);
-            newsListContainer.appendChild(newsItem);
+            // Aktualizacja koloru trójkąta
+            voteButton.style.color = isVoteButtonGreen ? 'green' : 'black';
         });
-    };
-    const loadMoreNews = async () => {
-        currentPage++;
-        const moreNews = await fetchNews(currentPage);
-        displayNews(moreNews);
-    };
-    const voteUp = (index) => {
-        console.log(`Voted up for news ${index}`);
-    };
-    const hideNews = (newsItem) => {
-        newsItem.hidden = true;
-    };
-    const init = async () => {
-        const initialNews = await fetchNews(currentPage);
-        displayNews(initialNews);
 
-        const loadMoreButton = document.getElementById('load-more');
-        loadMoreButton.addEventListener('click', loadMoreNews);
-    };
-const highlightVoteButton = (voteButton) => {
-    if(document.getElementsByClassName("vote-up").style.color == 'black') {
-        document.getElementsByClassName("vote-up").style.color = 'green';
-    } 
-    else if(document.getElementsByClassName("vote-up").style.color == 'green') {
-        document.getElementsByClassName("vote-up").style.color = 'black';
-    }
-};
-init();
+        const hideButton = document.createElement('span');// Przycisk do ukrywania newsa
+        hideButton.classList.add('hide-button');
+        hideButton.innerHTML = 'Hide';
+        hideButton.addEventListener('click', () => hideNews(newsItem));
+
+        const newsTitle = document.createElement('div');// Tytuł i link do newsa
+        newsTitle.classList.add('news-title');
+        const newsLink = document.createElement('a');
+        newsLink.classList.add('news-link');
+        newsLink.href = news.url;
+        newsLink.target = '_blank';
+        newsLink.innerHTML =` <span>${index + 1} </span>`+ news.title;
+        newsTitle.appendChild(voteButton);
+        newsTitle.appendChild(newsLink);
+
+        const newsMeta = document.createElement('div');// Meta informacje o newsie (punkty, autor, liczba komentarzy)
+        newsMeta.classList.add('news-meta');
+        const newsMetaSpan = document.createElement('span');
+        newsMetaSpan.innerHTML = `Score: ${news.score} | Author: ${news.by} | Comments: ${news.descendants} | `;
+        newsMeta.appendChild(newsMetaSpan);
+        newsMeta.appendChild(hideButton);
+
+        newsItem.appendChild(newsTitle); // Dodanie elementów do struktury HTML
+        newsItem.appendChild(newsMeta);
+        newsListContainer.appendChild(newsItem);
+    });
+}
+async function loadMoreNews() {// Funkcja asynchroniczna do ładowania kolejnych newsów
+    currentPage++;
+    const moreNews = await fetchNews(currentPage);
+    displayNews(moreNews);
+}
+function voteUp(index) {// Funkcja do obsługi głosowania
+    console.log(`Voted up for news ${index}`);
+}
+function hideNews(newsItem) {// Funkcja do ukrywania newsa (Komentarz wygenerowany przez chatGPT)
+    newsItem.hidden = true;
+}
+async function init() {// Inicjalizacja strony
+    const initialNews = await fetchNews(currentPage);
+    displayNews(initialNews);
+
+    const loadMoreButton = document.getElementById('load-more');
+    loadMoreButton.addEventListener('click', loadMoreNews);
+}
+
+init();// Wywołanie inicjalizacji po załadowaniu strony
